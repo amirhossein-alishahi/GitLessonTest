93613f3815d505abfd431a734721a52ef0fb3f7f
Alishahi <a.h.alishahi.cs@gmail.com>
2023-05-18 19:41:25 +0330

    adding reader to code1.py

--- code1.py
+++ code1.py
@@ -1,1 +1,11 @@
-print("hello")
+import csv
+
+csvFile = open('data.csv')
+
+csvReader = csv.DictReader(csvFile)
+
+listOfDict = []
+
+for item in csvReader:
+    listOfDict.append(item)
+    print(item)
